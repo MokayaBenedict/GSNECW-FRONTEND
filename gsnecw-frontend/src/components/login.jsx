1d75023ffc1aac3e39ff9b892ca53b9f2efba16c
Benedict <mokayaakivatsi@gmail.com>
2024-07-28 20:46:20 +0300

    started on signup page

---
+++ gsnecw-frontend/src/components/login.jsx
@@ -0,0 +1,41 @@
+// import React from 'react'
+
+
+// import './App.css'
+
+// function LoginPage() {
+
+
+//     return (
+      
+//             <form action="#" id="Login-form">
+//                  <label className="Formlabel">Name</label>
+//                  <br></br>
+//                 <input type='text' placeholder='JohnDoe' className='nameofplayer'></input>
+//                 <br></br>
+//                 <label className="Formlabel">Email</label>
+//                 <br></br>
+//                 <input type="email" placeholder="e.g JohnDoe@gmail.com" className="textbox" id="input-email" required/>
+//                 <br/><br/>
+//                 <label className="Formlabel">Password</label>
+//                 <br></br>
+               
+//                 <input type="password" placeholder="Password" className="textbox" id="input-password" required/>
+//                 <br/><br/>
+               
+//                 <button  className="button-login" 
+//                 id="button-login">Continue</button>
+                
+                
+                
+//                 <div><pre>
+//                 <a href="forgot_password.html" className="forgotpassword">Forgot Password?</a>
+                
+//                 <a href="signup.html" className="signup"id ="su"> Login Instead</a>
+//                 </pre></div>
+//             </form>
+        
+//     );
+// }
+
+// export default LoginPage;
