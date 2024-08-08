4900a34de34303987a5d8545581af865770a53f1
Benedict <mokayaakivatsi@gmail.com>
2024-08-08 14:21:12 +0300

    started on header

---
+++ gsnecw-frontend/src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+//import './footer.css';
+
+const Footer = ({ content }) => {
+  return (
+   
+        <header className="header">
+          <h1>About Us
+            
+          </h1>
+        </header>
+      );
+    };
+
+export default Footer;
