7876440f215b42c9e38946d541583b82aa3d37cd
Joy <mbuvimumbuambua99@gmail.com>
2024-08-14 11:48:46 +0300

    added a logout button

--- gsnecw-frontend/src/components/header.jsx
+++ gsnecw-frontend/src/components/header.jsx
@@ -1,19 +1,45 @@
 import React from 'react';
-import { AiOutlineShoppingCart, AiOutlineHeart } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { AiOutlineShoppingCart, AiOutlineHeart, AiOutlineLogout } from 'react-icons/ai';
+import { Link, useNavigate } from 'react-router-dom';
 import './header.css';
 
 const Header = ({ children }) => {
+  const navigate = useNavigate();  
+
+  const handleLogout = async () => {
+    try {
+      const token = localStorage.getItem('authToken');
+      if (token) {
+        await fetch('http://127.0.0.1:5000/logout', {
+          method: 'POST',
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+
+        localStorage.removeItem('authToken');
+
+        navigate('/login');
+      }
+    } catch (error) {
+      console.error('Error logging out:', error);
+      
+    }
+  };
+
   return (
     <header className="header-top">
       {children}
       <div className="header-icons">
         <Link to="/cart" className="icon-link">
-          <AiOutlineShoppingCart  title="Cart" />
+          <AiOutlineShoppingCart title="Cart" />
         </Link>
         <Link to="/favorites" className="icon-link">
-          <AiOutlineHeart  title="Favorites" />
+          <AiOutlineHeart title="Favorites" />
         </Link>
+        <span className="icon-link" onClick={handleLogout}>
+          <AiOutlineLogout title="Logout" />
+        </span>
       </div>
     </header>
   );
