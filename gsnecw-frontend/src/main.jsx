9b3bf98c43981d2b17f0a3abd28ae755d3748b3d
Kymaany <melvinkimani81@gmail.com>
2024-08-15 20:45:32 +0300

    added order history

--- gsnecw-frontend/src/main.jsx
+++ gsnecw-frontend/src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-
+import { AuthProvider } from "./context/AppContext";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
